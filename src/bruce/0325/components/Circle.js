004367bf66fbf65f09ca9be55d01a8ca9d583ecb
brucetseng <bruce.tseng@bytedance.com>
2021-03-25 21:41:12 +0800

    prettier
    Change-Id: Iadedef0e441c0042893187e71923b943f4c691a1

---
+++ src/bruce/0325/components/Circle.js
@@ -0,0 +1,13 @@
+import React from "react";
+import circle from "../img/cross.svg";
+import "../css/Global.css";
+
+function Circle() {
+  return (
+    <div className="boxImg">
+      <img src={circle}></img>
+    </div>
+  );
+}
+
+export default Circle;
--- src/bruce/Btn1.js
+++ src/bruce/Btn1.js
@@ -1,10 +1,8 @@
-import React from 'react'
+import React from "react";
 
 // Btn1 組件
-function Btn1 ({ pressFunc, a1 }) {
-  return <button onClick={pressFunc}>按我加一 {a1}</button>
+function Btn1({ pressFunc, a1 }) {
+  return <button onClick={pressFunc}>按我加一 {a1}</button>;
 }
 
-
-
-export default Btn1
+export default Btn1;
